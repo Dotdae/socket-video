3ae1fd91c290723e4fe194c8774f6320d47cac5d
Dotdae <chriiswho@gmail.com>
2022-09-15 14:07:42 -0600

    The start of something

---
+++ README.md
@@ -0,0 +1,1 @@
+# Node JS Video Socket App =)
---
+++ src/public/js/index.js
@@ -0,0 +1,97 @@
+// Load iframe tag.
+
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+
+var fistScriptTag = document.getElementsByTagName('script')[0];
+fistScriptTag.parentNode.insertBefore(tag, fistScriptTag);
+
+// Create the player.
+
+var player;
+
+function onYouTubeIframeAPIReady(){
+
+    player = new YT.Player('player', {
+
+        height: '560',
+        width: '1040',
+        videoId: '30MN4Mk2sQY',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        },
+        playerVars: {
+            'controls': 0,
+            'disablekb': 1,
+            'modestbranding': 1,
+            'rel': 0,
+            'showinfo': 0
+        }
+
+    });
+
+}
+
+
+// This function runs when the video is playing.
+
+function onPlayerReady(event){
+    progressBarLoop();
+}
+
+// This function listen the events on the video.
+
+function onPlayerStateChange(event){
+
+    if(event.data == 1) console.log('Play!')
+    if(event.data == 2) console.log('Pause!')
+
+}
+
+// Buttons listeners.
+
+var playButton = document.getElementById('play');
+var pauseButton = document.getElementById('pause');
+
+playButton.addEventListener('click', () =>{
+
+    player.playVideo();
+    console.log('Play!')
+
+});
+
+pauseButton.addEventListener('click', () =>{
+
+    player.pauseVideo();
+    console.log('Pause!')
+
+});
+
+
+// Progress bar.
+
+function progressBarLoop(){
+
+    var progressBar = $('.progressBar');
+    var square = $('#square');
+
+    progressBar.click(function(event){
+
+        var divOffset = $(this).offset();
+        console.log((event.pageX - divOffset.left)/765*videoLenght);
+
+    });
+
+    setInterval(function(){
+        
+        if(player == null || progressBar == null){
+            return;
+        }
+
+        var fraction = player.getCurrentTime()/player.getDuration()*100;
+        square.css('left', fraction.toString() + '%')
+
+    }, 200)
+
+}
